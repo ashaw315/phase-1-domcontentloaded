e3934ced1cbf51a258b296c7f742f25963d8c96f
Adam <ashaw315@gmail.com>
2021-11-28 10:14:10 -0500

    commit tree e4369d254409a7db68d61b089f9bbceb447d61d9

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    function updateText(text) {
+        let p = document.querySelector('#text')
+        p.textContent = 'This is really cool!'
+    }
+    updateText();
+})
+
+
+ // let p = document.createElement('p')
+    // p.textContent = 'This is really cool!'
+    // document.querySelector('#text').appendChild(p);
+    // console.log(document.querySelector('p'))
